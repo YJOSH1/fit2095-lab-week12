801442923473d598ffb634dc9a603b6db67b4e0e
yjosh1 <84948881+yjosh1@users.noreply.github.com>
2022-10-19 21:30:36 +1100

    completed task 1 and part of 2

--- server.js
+++ server.js
@@ -6,26 +6,22 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const path = require('path');
 
+const {Translate} = require('@google-cloud/translate').v2;
+const translate = new Translate();
+
 app.use('/', express.static(path.join(__dirname, 'dist/fit2095-lab-week12')));
 
+async function translateText(text, target) {
+    let [translated] = await translate.translate(text, target);
+    return translated
+};
 
 io.on('connection', function(socket) {
     console.log('new connection made, ' + socket.id);
 
-    socket.emit('onAuction', auction);
-
-    socket.on('auction', (localAuction) => {
-        auction = localAuction;
-        auction.isBidActive = true;
-        io.emit('onAuction', auction);
-    });
-
-    socket.on('bid', (bid) => {
-        io.emit('onBid', bid);
-        if (auction.targetPrice <= bid.value) {
-            auction.isBidActive = false;
-            io.emit('onAuction', auction)            
-        }
+    socket.on('translate', async (translateObject) => {
+        let translatedText = await translateText(translateObject.text, translateObject.target);
+        socket.emit('onTranslation', translatedText);
     });
 });
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ENVIRONMENT_INITIALIZER } from '@angular/core';
+import { io } from 'socket.io-client';
+
+interface translation {
+  from: string;
+  to: string;
+  orignal: string;
+  translated: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fit2095-lab-week12';
+
+  socket:any;
+
+  text: string = '';
+  target: string = '';
+  translated: string = '';
+  translations: any[] = []
+
+  ngOnInit(): void {
+    this.socket = io();
+  };
+
+  requestTranslation() {
+    let translateObject = {
+      text: this.text,
+      target: this.target    
+    }
+    console.log(translateObject);
+    this.socket.emit('translate', translateObject);
+
+    this.socket.on('onTranslation', (translatedText: any) => {
+      this.translations.push({
+        from: 'EN',
+        to: this.target,
+        orignal: this.text,
+        translated: translatedText
+      });
+    });
+  }
 }
